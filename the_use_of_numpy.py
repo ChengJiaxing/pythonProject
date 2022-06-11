50a93cc539c9aa6d2516ba7d612a391bd38fc8b9
ChengJiaxing <74436448+ChengJiaxing@users.noreply.github.com>
2022-06-11 15:57:54 +0800

    new

---
+++ the_use_of_numpy.py
@@ -0,0 +1,19 @@
+import numpy as np
+a=[1,2,3,4]
+b=np.array([1,2,3,4])
+# print(a)
+# print(b)
+# print(type(a))
+# print(type(b))
+# print(a[1])
+# print(b[1])
+# print(a[0:2])
+# print(b[0:2])
+c=a*2
+d=b*2
+# print(c)
+# print(d)
+e=[[1,2],[3,4],[5,6]]
+f=np.array([[1,2],[3,4],[5,6]])
+print(e)
+print(f)
