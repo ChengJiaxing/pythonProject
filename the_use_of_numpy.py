80dbd97a8b0c5a7797420e4130c341b713752194
ChengJiaxing <74436448+ChengJiaxing@users.noreply.github.com>
2022-06-11 16:11:00 +0800

    new

--- the_use_of_numpy.py
+++ the_use_of_numpy.py
@@ -1,6 +1,6 @@
 import numpy as np
-a=[1,2,3,4]
-b=np.array([1,2,3,4])
+# a=[1,2,3,4]
+# b=np.array([1,2,3,4])
 # print(a)
 # print(b)
 # print(type(a))
@@ -9,11 +9,27 @@ b=np.array([1,2,3,4])
 # print(b[1])
 # print(a[0:2])
 # print(b[0:2])
-c=a*2
-d=b*2
-# print(c)
-# print(d)
-e=[[1,2],[3,4],[5,6]]
-f=np.array([[1,2],[3,4],[5,6]])
-print(e)
-print(f)
+# c=a*2
+# d=b*2
+# # print(c)
+# # print(d)
+# e=[[1,2],[3,4],[5,6]]
+# f=np.array([[1,2],[3,4],[5,6]])
+# print(e)
+# print(f)
+# b=np.array([1,2,3,4])
+# f=np.array([[1,2],[3,4],[5,6]])
+x=np.arange(5)
+print(x)
+y=np.arange(5,10)
+print(y)
+z=np.arange(5,10,0.5)
+print(z)
+a=np.random.randn(3)
+print(a)
+a=np.random.rand(3)
+print(a)
+a=np.arange(12).reshape(3,4)
+print(a)
+a=np.random.randint(0,10,(4,4))
+print(a)
