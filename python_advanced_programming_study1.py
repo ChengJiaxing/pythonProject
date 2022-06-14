17810687cdd9a06eaa928109227fc11e65e8bd07
ChengJiaxing <74436448+ChengJiaxing@users.noreply.github.com>
2022-06-14 13:00:44 +0800

    Merge remote-tracking branch 'origin/master'

--- python_advanced_programming_study1.py
+++ python_advanced_programming_study1.py
@@ -128,21 +128,98 @@
 #         super().__init__(arg)
 # MyClass(10)
 
-class RevealAccess(object):
-    def __init__(self,initval=None,name="var"):
-        self.val=initval
-        self.name=name
-    def __get(self,obj,objtype):
-        print('Retrieving',self.name)
-        return self.val
-    def __set__(self,obj,val):
-        print('Updating',self.name)
-        self.val=val
-class MyClass(object):
-    x=RevealAccess(10,'var "x"')
-    y=5
-m=MyClass()
-print(m.x)
+# class RevealAccess(object):
+#     def __init__(self,initval=None,name="var"):
+#         self.val=initval
+#         self.name=name
+#     def __get(self,obj,objtype):
+#         print('Retrieving',self.name)
+#         return self.val
+#     def __set__(self,obj,val):
+#         print('Updating',self.name)
+#         self.val=val
+# class MyClass(object):
+#     x=RevealAccess(10,'var "x"')
+#     y=5
+# m=MyClass()
+# m.x=20
+
+# class InitOnAccess:
+#     def __init__(self,klass,*args,**kwargs):
+#         self.klass=klass
+#         self.args=args
+#         self.kwargs=kwargs
+#         self.__initialized=None
+#     def __get__(self,instance,owner):
+#         if self.__initialized is None:
+#             print('initialized!')
+#             self.__initialized=self.klass(*self.args,**self.kwargs)
+#         else:
+#             print('cached!')
+#         return self.__initialized
+
+
+# def short_repr(cls):
+#     cls.__repr__ = lambda self: super(cls, self).__repr__()[:8]
+#     return cls
+#
+#
+# @short_repr
+# class ClassWithRelativelyLongName:
+#     pass
+# ClassWithRelativelyLongName()
+
+# from math import hypot
+# class Vector:
+#     def __init__(self,x=0,y=0):
+#         self.x=x
+#         self.y=y
+#     def __repr__(self):
+#         return 'Vector(%r,%r)'%(self.x,self.y)
+#     def __abs__(self):
+#         return hypot(self.x,self.y)
+#     def __bool__(self):
+#         return bool(self.x or self.y)
+#     def __add__(self, other):
+#         x=self.x+other.x
+#         y=self.y+other.y
+#         return Vector(x,y)
+#     def __mul__(self,scalar):
+#         return Vector(self.x*self.scalar,self.y*scalar)
+
+# symbols = '$¢£¥€¤'
+# codes = []
+# for symbol in symbols:
+#   codes.append(ord(symbol))
+# print(codes)
+# codes=[ord(symbol) for symbol in symbols]
+# print(codes)
+#
+# x='ABC'
+# dummy=[ord(x) for x in x]
+# print(x)
+# print(dummy)
+symbols = '$¢£¥€¤'
+# beyond_ascii=[ord(s) for s in symbols if ord(s)>127]
+# beyond_ascii=[ord(s) for s in symbols if ord(s)>164]
+# print(beyond_ascii)
+# print(beyond_ascii)
+# beyond_ascii=list(filter(lambda c: c>127,map(ord,symbols)))
+# beyond_ascii=list(filter(lambda c: c>164,map(ord,symbols)))
+# print(beyond_ascii)
+
+# colors=['black','white']
+# sizes=['S','M','L']
+# tshirts=[(color,size) for color in colors for size in sizes]
+# print(tshirts)
+#
+# tshirts=[(size,color) for size in sizes for color in colors]
+# print(tshirts)
+# subjects=['math','english']
+# students=['a','b','c']
+# student_subject=[(subject,student) for student in students for subject in subjects]
+# print(student_subject)
+# print(student_subject[0])
 
 
 
